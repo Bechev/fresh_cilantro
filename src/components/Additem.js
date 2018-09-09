9d9340cfb8dbc84df777569c85972db65598d569
Bechev <bertrand.chevalier@gmail.com>
2018-09-09 15:16:57 -0400

    Add Request item functionnality to the website

--- src/components/Additem.js
+++ src/components/Additem.js
@@ -1,20 +1,110 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 
-export default class Additem extends Component {
+class Additem extends Component {
+    constructor(){
+        super()
+        this.state = {
+            item:{
+                id: "",
+                name: "",
+                brand: "",
+                description: ""
+            }
+        }
+    }
+
+    componentWillMount(){
+        this.assignItemID()
+    }
+
+    createItemID() {
+        return (
+            this.props.items[this.props.items.length -1].id + 1
+        )
+    }
+
+    assignItemID = () => {
+        let itemID = this.createItemID()
+        this.setState ({
+            item: {
+                ...this.state.item,
+                id: itemID
+            }
+        })
+    }
+    
+
+    handleNameChange = (event) => {
+        event.preventDefault()
+        this.setState ({
+            item: {
+                ...this.state.item,
+                name: event.target.value
+            }
+        })
+    }
+
+    handleBrandChange = (event) => {
+        event.preventDefault()
+        this.setState ({
+            item: {
+                ...this.state.item,
+                brand: event.target.value
+            }
+        })
+    }
+
+    handleDescriptionChange = (event) => {
+        event.preventDefault()
+        this.setState ({
+            item: {
+                ...this.state.item,
+                description: event.target.value
+            }
+        })
+    }
+
+    handleClick = (event) =>{
+        this.props.addItem(this.state.item)
+    }
 
     render() {
         return (
             <div>
-                <header className="header">Add Item</header>
-                <form>
-                    <label> Name:
-                    <input type="text" name="name" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
+            <header className="header">Add Item</header>
+            <form className="item">
+                <label> Name:
+                <input type="text" name="name" placeholder="Item name"  onChange={this.handleNameChange} />
+                </label>
+                <label> Brand:
+                <input type="text" name="brand" placeholder="Item Brand"  onChange={this.handleBrandChange}/>
+                </label>
+                <label> Description:
+                <textarea  name="description" placeholder="Item Description"  onChange={this.handleDescriptionChange}/>
+                </label>
+                <Link to={`items/${this.state.item.id}`}>
+                <input type="button" value="Request item" onClick={this.handleClick} />
+                </Link>
+            </form>
+        </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  }
+}
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: (item) => dispatch({type: 'ADD_ITEM', payload: item}),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Additem);
